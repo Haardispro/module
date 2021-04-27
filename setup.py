0611b414fbeddeb694af2a3d2e8e9ec4d2c86205
Haardispro <62053026+Haardispro@users.noreply.github.com>
2021-04-27 17:14:30 +0530

    Commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup
+
+setuptools.setup(
+     name='pasttense',  
+     version='0.1',
+     scripts=['pastense'] ,
+     author="Haard Majmudar",
+     author_email="haardmajmudar2827@gmail.com",
+     description="test",
+     
+     url="https://github.com/javatechy/dokr",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+     ],
+ )
